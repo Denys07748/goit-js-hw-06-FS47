4a8d56193beb5c248983244c7c44ae94e80f66be
Denys07748 <denis07748@gmail.com>
2022-05-22 23:03:29 +0300

    додає завдання 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,36 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('ul');
+
+// 1 спосіб
+
+// const elements = ingredients.map(ingredient => {
+//   const ingredientEl = document.createElement('li');
+//   ingredientEl.textContent = ingredient;
+//   ingredientEl.classList.add('item');
+
+//   return ingredientEl;
+// });
+
+// console.log(elements);
+
+// ingredientsEl.append(...elements);
+
+// 2 спосіб
+
+const makeIngredients = constituents => {
+  return constituents.map(constituent => {
+  const ingredientEl = document.createElement('li');
+  ingredientEl.textContent = constituent;
+  ingredientEl.classList.add('item');
+
+  return ingredientEl;
+});
+}
+
+const elements = makeIngredients(ingredients);
+console.log(elements);
+
+ingredientsEl.append(...elements);
