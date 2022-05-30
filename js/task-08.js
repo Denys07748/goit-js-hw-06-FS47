983fafbba1427b9f7e153476ce51245c896633d6
Denys07748 <denis07748@gmail.com>
2022-05-30 21:26:52 +0300

    Додає завдання 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,31 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    const isValidForm = [...formElements]
+    .filter((el) => el.tagName === 'INPUT')
+    .some(el => el.value === '');
+
+    if(isValidForm) {
+      alert('Будь ласка, заповніть всі поля;)');
+    }
+
+    const formData = {
+        email,
+        password,
+    };
+
+
+    console.log(formData);
+    event.currentTarget.reset();
+}
 
