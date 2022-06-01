7b830bb812817e376d523da505f490358deb8bbe
Denys07748 <denis07748@gmail.com>
2022-06-01 19:01:12 +0300

    додає завдання 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bgColorEl = document.querySelector('.color');
+const buttonEl = document.querySelector('.change-color');
+const bodyEl = document.body;
+
+buttonEl.addEventListener('click', onChengeBgColor);
+bodyEl.addEventListener('change', onChengeBgColor);
+
+function onChengeBgColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+
+    bgColorEl.textContent = bodyEl.style.backgroundColor;
+}
