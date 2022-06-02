e28c471051a19e1a94ef755ff6ab5b527386acba
Denys07748 <denis07748@gmail.com>
2022-06-02 20:11:27 +0300

    додає завдання 10 (частково)

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  input: document.querySelector('input'),
+  createBtn: document.querySelector('button[data-create'),
+  destroyBtn: document.querySelector('button[data-destoy'),
+  boxes: document.querySelector('#boxes'),
+}
+const amount = refs.input.value;
+// refs.input.addEventListener('input', );
+
+refs.createBtn.addEventListener('click', onClickWrapper);
+refs.destroyBtn.addEventListener('click', onDestroyBoxes);
+
+function onClickWrapper(e) {
+  onCreateBoxes(e, amount);
+}
+
+function onCreateBoxes(e, amount) {
+  
+ const divEl = document.createElement('div');
+  const widthDiv = divEl.width;
+  const heightDiv = divEl.height;
+
+  widthDiv
+
+}
+
+function onDestroyBoxes() {
+
+}
