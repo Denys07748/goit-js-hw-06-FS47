d8d6522e97d69628cd04efa509e7b6cd19985074
Denys07748 <denis07748@gmail.com>
2022-06-03 20:51:40 +0300

    додає завдання 10

--- js/task-10.js
+++ js/task-10.js
@@ -4,30 +4,38 @@ function getRandomHexColor() {
 
 const refs = {
   input: document.querySelector('input'),
-  createBtn: document.querySelector('button[data-create'),
-  destroyBtn: document.querySelector('button[data-destoy'),
+  createBtn: document.querySelector('button[data-create]'),
+  destroyBtn: document.querySelector('button[data-destroy]'),
   boxes: document.querySelector('#boxes'),
 }
-const amount = refs.input.value;
-// refs.input.addEventListener('input', );
 
 refs.createBtn.addEventListener('click', onClickWrapper);
 refs.destroyBtn.addEventListener('click', onDestroyBoxes);
 
 function onClickWrapper(e) {
-  onCreateBoxes(e, amount);
+  onCreateBoxes(e, refs.input.value);
 }
 
 function onCreateBoxes(e, amount) {
+  const boxesArray = [];
+  let sizeDivEl = 20;
+
+  for (let i = 0; i < amount; i += 1) {
+    const divEl = document.createElement('div');
   
- const divEl = document.createElement('div');
-  const widthDiv = divEl.width;
-  const heightDiv = divEl.height;
+    sizeDivEl += 10;
 
-  widthDiv
+    divEl.style.width = `${sizeDivEl}px`;
+    divEl.style.height = `${sizeDivEl}px`;
+    divEl.style.marginTop = '10px';
+    divEl.style.backgroundColor = getRandomHexColor();
 
+    boxesArray.push(divEl);
+  }
+
+  refs.boxes.append(...boxesArray);
 }
 
 function onDestroyBoxes() {
-
+  refs.boxes.innerHTML = '';
 }
